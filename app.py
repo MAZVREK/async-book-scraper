9d072af172f613dcd22385f6196a0d9584b470c9
Kamil Mazurek <87046210+MAZVREK@users.noreply.github.com>
2022-02-03 23:06:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import aiohttp
+import async_timeout
+import asyncio
 import requests
 
 from pages.all_books_page import AllBooksPage
@@ -7,11 +10,27 @@ PAGE_CONTENT = requests.get('https://books.toscrape.com').content
 
 page = AllBooksPage(PAGE_CONTENT)
 
+loop = asyncio.get_event_loop()
+
 books = page.books
 
-for page_num in range(1, page.page_count):
-    url = f'https://books.toscrape.com/catalogue/page-{page_num+1}.html'
-    page_content = requests.get(url).content
+async def fetch_page(session, url):
+    async with async_timeout.timeout(10):
+        async with session.get(url, ssl=False) as response:
+            return await response.text()
+
+async def get_multiple_pages(loop, *urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
+
+urls = [f'https://books.toscrape.com/catalogue/page-{page_num+1}.html' for page_num in range(1, page.page_count)]
+pages = loop.run_until_complete(get_multiple_pages(loop, *urls))
+
+for page_content in pages:
     page = AllBooksPage(page_content)
     books.extend(page.books)
 
